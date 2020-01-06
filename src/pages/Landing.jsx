560dd67e05b405461e6b5100074ea75460d48b30
Mourad Aouinat <mourad.eyes@gmail.com>
2020-01-06 18:47:42 +0100

    Add 'Call to Action' section

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
 import HowTo from '../components/HowTo'
+import CallToAction from '../components/CallToAction'
 
 const Landing = () => (
   <div>
     <Header />
     <HowTo />
+    <CallToAction />
   </div>
 )
 
