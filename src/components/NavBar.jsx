8d662a0accbc33cd0a74e983cc0560d12fb07f34
Mourad <mourad.eyes@gmail.com>
2020-01-10 11:06:14 +0100

    feat: Add sign out

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,10 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/prop-types */
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { Dropdown } from 'react-bootstrap'
+import { signOut } from '../firebase'
 import { UserContext } from '../contexts/UserProvider'
 import logo from '../img/logo_white.svg'
 import styles from '../styles/NavBar.module.scss'
@@ -14,10 +18,17 @@ const NavBar = () => {
         <Link to="/">
           <img src={logo} className={styles.logo} alt="Grabit" />
         </Link>
-        <div>
-          <span className={styles.userName}>{user.displayName}</span>
-          <img src={user.photoURL} className={styles.userImage} alt="Avatar" />
-        </div>
+        <Dropdown>
+          <Dropdown.Toggle variant="transparent">
+            <span className={styles.userName}>{user.displayName}</span>
+            <img src={user.photoURL} className={styles.userImage} alt="Avatar" />
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <span className="dropdown-item" onClick={signOut}>
+              Sign Out
+            </span>
+          </Dropdown.Menu>
+        </Dropdown>
       </div>
     </nav>
   )
