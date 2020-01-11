9393d0133f25c456b8e9d7698fbd0d998e3f379a
Mourad <mourad.eyes@gmail.com>
2020-01-11 17:22:32 +0100

    refactor: landing page styles

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
-import styles from '../styles/CallToAction.module.css'
 import send from '../img/send.svg'
 
 const CallToAction = () => {
   return (
-    <div className={styles.cta}>
-      <h2 className={styles['cta-heading']}>Ready to order?</h2>
-      <div className={styles['cta-paragraph-wrapper']}>
-        <p className={styles['cta-paragraph']}>
+    <div className="cta">
+      <h2 className="cta__heading">Ready to order?</h2>
+      <div className="cta__paragraph-wrapper">
+        <p className="cta__paragraph">
           Browse local restaurants and businesses available in your area for delivery by entering
           your address below.
         </p>
       </div>
-      <div className={styles.input}>
-        <input type="text" className={styles.control} placeholder="mail@exemple.com" />
-        <button type="button" className={styles.send}>
+      <div className="cta__input">
+        <input type="text" className="cta__control" placeholder="mail@exemple.com" />
+        <button type="button" className="cta__send">
           Send
           <img className="ml-3" src={send} alt="send" width="10" />
         </button>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import styles from '../styles/Footer.module.css'
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <span className={styles.legal}>&copy; 2019 Grabit</span>{' '}
-      <ul className={styles.list}>
-        <li className={styles['list-item']}>Terms</li>
-        <li className={styles['list-item']}>Privacy Policy</li>
+    <footer className="footer">
+      <span>&copy; 2019 Grabit</span>{' '}
+      <ul className="footer__legal">
+        <li className="footer__legal-item">Terms</li>
+        <li className="footer__legal-item">Privacy Policy</li>
       </ul>
     </footer>
   )
