22f3eacfa1713aea4e03f60a9693f1b60d14d749
Mourad <mourad.eyes@gmail.com>
2020-01-16 21:29:29 +0100

    Add hamburger menu for small devices

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,27 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/prop-types */
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { signOut } from '../firebase'
 import { UserContext } from '../contexts/UserProvider'
 import { ReactComponent as Logo } from '../img/logo_white.svg'
 import { ReactComponent as DropDown } from '../img/caret.svg'
+import { ReactComponent as Burger } from '../img/burger.svg'
+import NavMobile from './NavMobile'
 
 const NavBar = () => {
   const { displayName, photoURL } = useContext(UserContext)
+  const [isNavOpen, setIsNavOpen] = useState(false)
+
+  const onClose = () => {
+    setIsNavOpen(false)
+  }
 
   return (
     <nav className="navbar">
       <div className="container navbar__items">
+        <Burger className="burger__menu" onClick={() => setIsNavOpen(!isNavOpen)} />
         <Link to="/">
           <Logo className="navbar__logo" />
         </Link>
@@ -36,6 +44,7 @@ const NavBar = () => {
           </div>
         </div>
       </div>
+      <NavMobile toggle={isNavOpen} onClose={onClose} />
     </nav>
   )
 }
---
+++ src/components/NavMobile.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { ReactComponent as User } from '../img/user.svg'
+import { ReactComponent as Bell } from '../img/bell.svg'
+import { ReactComponent as Faq } from '../img/faq.svg'
+import { ReactComponent as Home } from '../img/home.svg'
+
+const NavMobile = ({ toggle, onClose }) => {
+  return (
+    <div className={`nav-mobile ${toggle ? 'nav-open' : ''}`}>
+      <ul className="nav-mobile__list">
+        <NavLink
+          exact
+          to="/dashboard/"
+          activeClassName="mobile-selected"
+          className="nav-mobile__list__link"
+          onClick={() => onClose()}
+        >
+          <li className="nav-mobile__list__item">
+            <User className="nav-mobile__list__icon mr-4" />
+            Profile Settings
+          </li>
+        </NavLink>
+        <NavLink
+          to="/dashboard/requests"
+          activeClassName="mobile-selected"
+          className="nav-mobile__list__link"
+          onClick={() => onClose()}
+        >
+          <li className="nav-mobile__list__item">
+            <Bell className="nav-mobile__list__icon mr-4" /> Requests
+          </li>
+        </NavLink>
+        <NavLink
+          to="/dashboard/address"
+          activeClassName="mobile-selected"
+          className="nav-mobile__list__link"
+          onClick={() => onClose()}
+        >
+          <li className="nav-mobile__list__item">
+            <Home className="nav-mobile__list__icon mr-4" />
+            Address
+          </li>
+        </NavLink>
+        <NavLink
+          to="/dashboard/faq"
+          activeClassName="mobile-selected"
+          className="nav-mobile__list__link"
+          onClick={() => onClose()}
+        >
+          <li className="nav-mobile__list__item">
+            <Faq className="nav-mobile__list__icon mr-4" />
+            FAQ
+          </li>
+        </NavLink>
+      </ul>
+    </div>
+  )
+}
+
+export default NavMobile
