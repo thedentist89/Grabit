5a64c0dafe9dda4eace123e645e2a7edc471ebf2
Mourad <mourad.eyes@gmail.com>
2020-01-16 14:54:56 +0100

    Refactor Dropdown menu and remove react-bootstrap dependancy

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,10 +3,10 @@
 /* eslint-disable react/prop-types */
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
-import { Dropdown } from 'react-bootstrap'
 import { signOut } from '../firebase'
 import { UserContext } from '../contexts/UserProvider'
 import { ReactComponent as Logo } from '../img/logo_white.svg'
+import { ReactComponent as DropDown } from '../img/caret.svg'
 
 const NavBar = () => {
   const user = useContext(UserContext)
@@ -17,17 +17,16 @@ const NavBar = () => {
         <Link to="/">
           <Logo className="navbar__logo" />
         </Link>
-        <Dropdown>
-          <Dropdown.Toggle variant="transparent">
-            <span className="navbar__user-name">{user.displayName}</span>
-            <img src={user.photoURL} className="navbar__user-image" alt="Avatar" />
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <span className="dropdown-item" onClick={signOut}>
+        <div className="drop-down">
+          <span className="navbar__user-name">{user.displayName}</span>
+          <img src={user.photoURL} className="navbar__user-image drop-down__button" alt="Avatar" />
+          <DropDown className="drop-down__icon" />
+          <div className="drop-down__content">
+            <span className="drop-down__item" onClick={signOut}>
               Sign Out
             </span>
-          </Dropdown.Menu>
-        </Dropdown>
+          </div>
+        </div>
       </div>
     </nav>
   )
